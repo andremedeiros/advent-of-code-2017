9e75fd568fde45476190a42406ecb32f0ca32260
Andre Medeiros <me@andremedeiros.info>
2017-12-09 12:19:26 -0500

    Add TODOs

--- 01/main.go
+++ 01/main.go
@@ -12,6 +12,7 @@ func main() {
 	scanner.Scan()
 
 	puzzle := strings.TrimSpace(scanner.Text())
+	// TODO: Try to avoid doing all this work ahead of time.
 	digits := getIntSlice(puzzle)
 
 	firstCaptcha := solveFirstCaptcha(digits)
