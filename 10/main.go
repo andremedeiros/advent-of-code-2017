b1555ed1d66300545af143c3e3125a4d337c5828
Andre Medeiros <me@andremedeiros.info>
2017-12-23 10:32:50 -0500

    Day 10, part 2

--- 10/main.go
+++ 10/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -17,7 +18,8 @@ func main() {
 		panic(err)
 	}
 
-	input := strings.TrimSpace(string(rawInput))
+	rawInput = bytes.TrimSpace(rawInput)
+	input := string(rawInput)
 
 	lengths := []int{}
 
@@ -26,8 +28,29 @@ func main() {
 		lengths = append(lengths, length)
 	}
 
+	// Part 1
 	list := NewList(0, 255)
-	fmt.Println(Hash(list, lengths))
+	fmt.Println(Hash(list, lengths, 1))
+
+	// Part 2
+	list = NewList(0, 255)
+	newLengths := []int{}
+	for _, b := range rawInput {
+		newLengths = append(newLengths, int(b))
+	}
+	newLengths = append(newLengths, []int{17, 31, 73, 47, 23}...)
+	sparseHash := Hash(list, newLengths, 64)
+
+	hash := ""
+	for block := 0; block < 16; block++ {
+		elem := sparseHash[block*16]
+		for i := (block * 16) + 1; i < (block*16)+16; i++ {
+			elem = elem ^ sparseHash[i]
+		}
+		hash = fmt.Sprintf("%s%x", hash, elem)
+	}
+
+	fmt.Println(hash)
 }
 
 // NewList generates an int list starting at `start` and ending at `end`
@@ -42,31 +65,33 @@ func NewList(start, end int) []int {
 }
 
 // Hash hashes the digit list with the lengths list provided
-func Hash(digits, lengths []int) []int {
+func Hash(digits, lengths []int, runs int) []int {
 	skip := 0
 	pos := 0
 
-	for _, length := range lengths {
-		start := pos
-		end := pos + length
+	for x := 0; x < runs; x++ {
+		for _, length := range lengths {
+			start := pos
+			end := pos + length
 
-		sublist := []int{}
-		for i := start; i < end; i++ {
-			sublist = append(sublist, digits[i%len(digits)])
+			sublist := []int{}
+			for i := start; i < end; i++ {
+				sublist = append(sublist, digits[i%len(digits)])
+			}
+
+			// Reverse the list
+			for i, j := 0, len(sublist)-1; i < j; i, j = i+1, j-1 {
+				sublist[i], sublist[j] = sublist[j], sublist[i]
+			}
+
+			// Put it back in place
+			for i, item := range sublist {
+				digits[(start+i)%len(digits)] = item
+			}
+
+			pos += skip + (end - start)
+			skip++
 		}
-
-		// Reverse the list
-		for i, j := 0, len(sublist)-1; i < j; i, j = i+1, j-1 {
-			sublist[i], sublist[j] = sublist[j], sublist[i]
-		}
-
-		// Put it back in place
-		for i, item := range sublist {
-			digits[(start+i)%len(digits)] = item
-		}
-
-		pos += skip + (end - start)
-		skip++
 	}
 
 	return digits
--- 10/main_test.go
+++ 10/main_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestHash(t *testing.T) {
 	digits := NewList(0, 4)
-	hashed := Hash(digits, []int{3, 4, 1, 5})
+	hashed := Hash(digits, []int{3, 4, 1, 5}, 1)
 
 	wanted := []int{3, 4, 2, 1, 0}
 	if !reflect.DeepEqual(wanted, hashed) {
